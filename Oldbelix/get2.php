da37f734090c3159a50e78983351207d45535b02
Hristo Dachev <hristo.dachev97@gmail.com>
2015-05-20 23:52:57 +0300

    update
    update

--- Oldbelix/get2.php
+++ Oldbelix/get2.php
@@ -2,6 +2,6 @@
 $json = $_POST['json'];
 
 $new = json_decode($json);
-echo 'Hello '. $new->FIRSTNAME.' ' .$new->LASTNAME. ' you were born ' .$new->DATE_OF_BIRTH;
+echo '<h1>Hello '. $new->FIRSTNAME.' ' .$new->LASTNAME. ' you were born ' .$new->DATE_OF_BIRTH.'</h1>';
 //echo $json;
 ?>
