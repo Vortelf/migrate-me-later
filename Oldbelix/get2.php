e7404116aeda036cb3f7710d4b284b21ab2182e9
Hristo Dachev <hristo.dachev97@gmail.com>
2015-05-21 00:00:25 +0300

    up
    up

--- Oldbelix/get2.php
+++ Oldbelix/get2.php
@@ -2,6 +2,8 @@
 $json = $_POST['json'];
 
 $new = json_decode($json);
-echo '<h1>Hello '. $new->FIRSTNAME.' ' .$new->LASTNAME. ' you were born ' .$new->DATE_OF_BIRTH.'</h1>';
-//echo $json;
+
+$arr2 = str_split($new->DATE_OF_BIRTH, 4);
+$year = (int)$arr2[0];
+echo '<h1>Hello '. $new->FIRSTNAME.' ' .$new->LASTNAME. ' you are ' . (int) (2015 - $year).' years old</h1>';
 ?>
